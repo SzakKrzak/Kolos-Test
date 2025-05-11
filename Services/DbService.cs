62aaa7ceb9fa08a67966638bdd1d487f934ba00d
Tymon <s29787@pjwstk.edu.pl>
2025-05-11 23:44:30 +0200

    Made template

---
+++ Services/DbService.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+using Kol_Test.DTOs;
+
+namespace Kol_Test.Services;
+
+public interface IDbService
+{
+}
+
+public class DbService(IConfiguration config) : IDbService
+{
+    
+}
